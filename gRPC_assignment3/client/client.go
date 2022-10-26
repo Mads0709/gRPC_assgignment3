62b2a59e6914d18ab6cd501bc52820eb9f80c3d6
mgan@itu.dk <mgan@itu.dk>
2022-10-26 15:06:13 +0200

    Users

--- gRPC_assignment3/client/client.go
+++ gRPC_assignment3/client/client.go
@@ -27,23 +27,36 @@ var (
 func main() {
 	// Parse the flags to get the port for the client
 	flag.Parse()
-
 	// Create a client
-	client := &Client{
-		name:        "user",
+	client1 := &Client{
+		name:        "Mads",
 		portNumber:  *clientPort,
 		vectorClock: "<0,0,0>",
 	}
 
+	client2 := &Client{
+		name:        "Mathias",
+		portNumber:  *clientPort,
+		vectorClock: "<0,0,0>",
+	}
+
+	/*client2 := &Client{
+		name:        "user2",
+		portNumber:  *clientPort,
+		vectorClock: "<0,0,0>",
+	}*/
+
 	// Wait for the client (user) to ask for the time
-	go waitForTimeRequest(client)
+	//log.Printf(client.name, client.portNumber, client.vectorClock)
+	go waitForTimeRequest1(client1)
+	go waitForTimeRequest2(client2)
 
 	for {
 
 	}
 }
 
-func waitForTimeRequest(client *Client) {
+func waitForTimeRequest1(client *Client) {
 	// Connect to the server
 	serverConnection, _ := connectToServer()
 
@@ -57,7 +70,32 @@ func waitForTimeRequest(client *Client) {
 		messageReturnMessage, err := serverConnection.ChatService(context.Background(), &proto.MessageFromClient{
 			Name:      client.name,
 			Msg:       "hej",
-			Timestamp: "12.40",
+			Timestamp: client.vectorClock,
+		})
+
+		if err != nil {
+			log.Printf(err.Error())
+		} else {
+			log.Printf("Server %s says the time is %s\n", messageReturnMessage.Name, messageReturnMessage.Timestamp)
+		}
+	}
+}
+
+func waitForTimeRequest2(client *Client) {
+	// Connect to the server
+	serverConnection, _ := connectToServer()
+
+	// Wait for input in the client terminal
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		input := scanner.Text()
+		log.Printf("Client asked for the message with input: %s\n", input)
+
+		// Ask the server for the time
+		messageReturnMessage, err := serverConnection.ChatService(context.Background(), &proto.MessageFromClient{
+			Name:      client.name,
+			Msg:       "hej",
+			Timestamp: client.vectorClock,
 		})
 
 		if err != nil {
